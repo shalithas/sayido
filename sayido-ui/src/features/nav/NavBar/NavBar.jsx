0eb686494fb39b59ae948a7855bb4c6189c4db79
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2020-03-30 01:19:05 +0800

    NavBar added

---
+++ sayido-ui/src/features/nav/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import { Menu } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
+
+const navBarStyle = {
+    backgroundColor: 'white',
+    position:'fixed',
+    left: 0,
+    top:0,
+    bottom: 0,
+    zIndex: 2500
+};
+
+const NavBar = () => {
+  return (
+    <Fragment>
+      <Menu pointing secondary vertical style={navBarStyle}>
+        <Menu.Item name='Dashboard' as={NavLink} to='/' exact />
+        <Menu.Item name='Guest List' as={NavLink} to='/guest-list' />
+        <Menu.Item name='friends' active={false} />
+      </Menu>
+    </Fragment>
+  );
+};
+
+export default NavBar;

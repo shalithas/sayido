67709ca6f4cf3965fec472ecd73d92ad94f8ca65
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2020-04-02 22:15:40 +0800

    input components added

---
+++ client/src/app/common/form/DateInput.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDatePicker  from 'react-datepicker'
+import { Form, Label } from 'semantic-ui-react';
+import 'react-datepicker/dist/react-datepicker.css';
+import toDate from 'date-fns/toDate';
+
+const DateInput = ({
+    input: {value, onChange, onBlur},
+    width,
+    placeholder,
+    meta: { touched, error },
+    ...rest
+  }) => {
+    return (
+      <Form.Field error={touched && !!error}>
+        <ReactDatePicker
+            {...rest}
+            placeholderText={placeholder}
+            selected={value
+              ? Object.prototype.toString.call(value) !== '[object Date]'
+                ? toDate(value)
+                : value
+              : null}
+            onChange={onChange}
+            onBlur={onBlur}
+            onChangeRaw={ e => e.preventDefault() }
+        />
+        {touched && error && (
+          <Label basic color='red'>
+            {error}
+          </Label>
+        )}
+      </Form.Field>
+    );
+  };
+
+export default DateInput
---
+++ client/src/app/common/form/SelectInput.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Select, Form, Label } from "semantic-ui-react";
+
+const SelectInput = ({
+  input,
+  width,
+  type,
+  placeholder,
+  multiple,
+  options,
+  meta: { touched, error }
+}) => {
+  return (
+    <Form.Field error={touched && !!error}>
+      <Select
+        {...input}
+        placeholder={placeholder}
+        type={type}
+        value={input.value || null}
+        onChange={(e, data) => input.onChange(data.value)}
+        options={options}
+        multiple={multiple}
+      />
+      {touched && error && (
+        <Label basic color='red'>
+          {error}
+        </Label>
+      )}
+    </Form.Field>
+  );
+};
+
+export default SelectInput;

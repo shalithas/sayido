2106f5f3b2b6e679923b6c939e041ba2e2035c91
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2020-04-05 00:13:36 +0800

    nav and body background colours updated

--- client/src/features/nav/NavBar/NavBar.jsx
+++ client/src/features/nav/NavBar/NavBar.jsx
@@ -3,12 +3,15 @@ import { Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 const navBarStyle = {
-    backgroundColor: 'white',
+    backgroundColor: '#fafbfc',
     position:'fixed',
     left: 0,
     top:0,
     bottom: 0,
-    zIndex: 100
+    zIndex: 100,
+    margin: 0,
+    borderRight: '1px solid #e1e4e8',
+    paddingTop: '40px'
 };
 
 const NavBar = () => {
@@ -17,7 +20,7 @@ const NavBar = () => {
       <Menu pointing secondary vertical style={navBarStyle}>
         <Menu.Item name='Dashboard' as={NavLink} to='/' exact />
         <Menu.Item name='Guest List' as={NavLink} to='/guests' exact />
-        <Menu.Item name='friends' active={false} />
+        <Menu.Item name='Payments' active={false} />
       </Menu>
     </Fragment>
   );

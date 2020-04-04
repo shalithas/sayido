2d047fd3d27bd6f3af0eb105a436459edba2b8a4
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2020-04-04 23:50:23 +0800

    toastr added

--- client/src/app/reducers/rootReducer.js
+++ client/src/app/reducers/rootReducer.js
@@ -2,13 +2,15 @@ import { combineReducers } from "redux";
 import guestReducer from "../../features/guest/guestReducer";
 import asyncReducer from "../../features/async/asyncReducer";
 import { reducer as formReducer } from "redux-form";
+import { reducer as toastreducer } from "react-redux-toastr";
 import modalReducer from "../../features/models/modalReducer";
 
 const rootReducer = combineReducers({
     guests: guestReducer,
     async: asyncReducer,
     form: formReducer,
-    modals: modalReducer
+    modals: modalReducer,
+    toastr: toastreducer
 });
 
 export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,25 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app/layout/App";
-import * as serviceWorker from "./serviceWorker";
-import { store } from "./app/store/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
+import './index.css';
+import App from './app/layout/App';
+import * as serviceWorker from './serviceWorker';
+import { store } from './app/store/store';
+import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr';
 
-const rootEl = document.getElementById("root");
+const rootEl = document.getElementById('root');
 
 let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
+        <ReduxToastr
+          position='bottom-right'
+          transitionIn='fadeIn'
+          transitionOut='fadeOut'
+        />
         <App />
       </BrowserRouter>
     </Provider>,
@@ -21,7 +28,7 @@ let render = () => {
 };
 
 if (module.hot) {
-  module.hot.accept("./app/layout/App", () => {
+  module.hot.accept('./app/layout/App', () => {
     setTimeout(render);
   });
 }

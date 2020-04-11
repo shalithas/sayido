8746f486fd6f4b41d571805ff77d9b42a960d87f
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2020-04-11 23:19:07 +0800

    icon attribute added to service

--- server/app/service/service.model.js
+++ server/app/service/service.model.js
@@ -5,12 +5,17 @@ export const serviceSchema = new Schema({
     name: {
         type: String,
         required: true
+    },
+    icon: {
+        type: String,
+        required: true
     }
 });
 
 export const validate = (service) => {
     const schema = {
-        name: Joi.string().required()
+        name: Joi.string().required(),
+        icon: Joi.string().required()
     };
 
     return Joi.validate(service, schema);

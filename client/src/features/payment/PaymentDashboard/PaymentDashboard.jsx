d8f22652e433eb87ef861b9a31ec8244d5250894
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2020-04-11 23:21:30 +0800

    payment listing page updated

--- client/src/features/payment/PaymentDashboard/PaymentDashboard.jsx
+++ client/src/features/payment/PaymentDashboard/PaymentDashboard.jsx
@@ -1,25 +1,36 @@
-import React, { Fragment } from 'react'
-import { Grid, Icon, Button } from 'semantic-ui-react'
-import PaymentList from '../PaymentList/PaymentList'
-import { Link } from 'react-router-dom'
+import React, { Fragment } from 'react';
+import { Grid, Icon, Button } from 'semantic-ui-react';
+import PaymentList from '../PaymentList/PaymentList';
+import { Link } from 'react-router-dom';
+import PaymentStats from '../PaymentStats/PaymentStats';
 
 const PaymentDashboard = () => {
-    return (
-        <Fragment>
-        <Grid>
+  return (
+    <Fragment>
+      <Grid>
+        <Grid.Row>
           <Grid.Column width={10}>
-            <h1>Guest List</h1>
+            <h1>Payments</h1>
           </Grid.Column>
           <Grid.Column width={6}>
             <Button primary floated='right' as={Link} to='/guests/new'>
               <Icon name='plus' /> Add payment
             </Button>
           </Grid.Column>
-        </Grid>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={10}>
+            <PaymentList />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <PaymentStats />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
 
-        <PaymentList />
-      </Fragment>
-    )
-}
+      
+    </Fragment>
+  );
+};
 
-export default PaymentDashboard
+export default PaymentDashboard;
--- client/src/features/payment/PaymentList/PaymentList.jsx
+++ client/src/features/payment/PaymentList/PaymentList.jsx
@@ -1,35 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
 import { fetchPayments } from '../paymentActions';
 import { connect } from 'react-redux';
 import LoadingIndicater from '../../../app/layout/LoadingIndicater';
+import '@fortawesome/fontawesome-free/js/all';
+import { Button, Item, Icon } from 'semantic-ui-react';
 
 class PaymentList extends Component {
-    componentWillMount(){
-        this.props.fetchPayments();
-    }
+  componentWillMount() {
+    this.props.fetchPayments();
+  }
 
-    render() {
-        const { payments, loading } = this.props;
-        console.log(payments);
-        if(loading)
-            return <LoadingIndicater />
-        return (
-            <div>
-                <h1>Payment list</h1>
-            </div>
-        )
-    }
+  render() {
+    const { payments, loading } = this.props;
+    if (loading) return <LoadingIndicater />;
+    return (
+      <Item.Group divided>
+        {payments.map((payment) => (
+          <Item>
+            <Icon as={Item.Image} className={`fa fa-4x fa-${payment.service.icon}`} />
+            <Item.Content>
+              <Item.Header as='a'>{payment.service.name}</Item.Header>
+              <Item.Description>
+                Mada a payment to{' '}
+                <a>
+                  <b>{payment.vendor.name}</b>
+                </a>{' '}
+                just now.
+              </Item.Description>
+            </Item.Content>
+            <Button animated>
+              <Button.Content visible primary>Edit</Button.Content>
+              <Button.Content hidden>
+                <Icon name='pencil' />
+              </Button.Content>
+            </Button>
+            <Button animated color='red'>
+              <Button.Content visible primary>Del</Button.Content>
+              <Button.Content hidden>
+                <Icon name='trash' />
+              </Button.Content>
+            </Button>
+          </Item>
+        ))}
+      </Item.Group>
+    );
+  }
 }
 
 const mapState = (state, ownProps) => {
-    return {
-        payments: state.payments.list,
-        loading: state.async.loading
-    }
-}
+  return {
+    payments: state.payments.list,
+    loading: state.async.loading,
+  };
+};
 
 const actions = {
-    fetchPayments
+  fetchPayments,
 };
 
 export default connect(mapState, actions)(PaymentList);

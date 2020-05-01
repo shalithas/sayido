fd8592baa16191876c2102cde0784632b0728c2a
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2020-05-01 16:51:16 +0800

    vendor route added

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -3,6 +3,7 @@ import guestController from '../app/guest/guest.controller';
 import paymentController from "../app/payment/payment.controller";
 import authController from "../app/user/auth.controller";
 import userController from "../app/user/user.controller";
+import vendorController from "../app/vendor/vendor.controller";
 
 export default app => {
     app.use(json());
@@ -10,4 +11,5 @@ export default app => {
     app.use('/api/payments', paymentController);
     app.use('/api/auth', authController);
     app.use('/api/users', userController);
+    app.use('/api/vendors', vendorController);
 }

79fa8a558b4c50bb799cf2cd1ea998da0627dcdd
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2020-05-01 16:52:19 +0800

    payment form updated to fetch vendor and populate

--- client/src/features/payment/PaymentForm.jsx/PaymentForm.jsx
+++ client/src/features/payment/PaymentForm.jsx/PaymentForm.jsx
@@ -6,15 +6,37 @@ import SelectInput from '../../../app/common/form/SelectInput';
 import { combineValidators, isRequired } from 'revalidate';
 import { closeModal } from '../../models/modalActions';
 import { connect } from 'react-redux';
+import { fetchVendors } from '../../vendor/vendorActions';
 
 const validate = combineValidators({
   amount: isRequired({ message: 'Payment amount is empty' }),
 });
 
+/**
+ * Prepares the options list for select elements
+ * @param {*} list 
+ */
+const prepareOptions = (list) => {
+  const vendorOptions = [];
+  list.forEach((vendor) => {
+    vendorOptions.push({
+      text: vendor.name,
+      value: vendor._id,
+      key: vendor._id,
+    });
+  });
+  return vendorOptions;
+};
+
 class PaymentForm extends Component {
+  componentDidMount() {
+    this.props.fetchVendors();
+  }
+
   render() {
     const options = [{ text: 'Test', value: 'test', key: 0 }];
-    const { invalid, submitting, pristine, closeModal } = this.props;
+    const { invalid, submitting, pristine, closeModal, vendors } = this.props;
+    const vendorOptions = prepareOptions(vendors);
     return (
       <Form autoComplete='off'>
         <Header color='teal' content='Payment Info' />
@@ -27,7 +49,7 @@ class PaymentForm extends Component {
         <Field
           component={SelectInput}
           name='vendor'
-          options={options}
+          options={vendorOptions}
           placeholder='Vendor'
         />
         <Field component={TextInput} name='amount' placeholder='Amount' />
@@ -52,15 +74,17 @@ class PaymentForm extends Component {
 }
 
 const mapState = (state, ownProps) => {
-
-
   return {
-
-  }
-}
-
-const actions = {
-  closeModal
+    vendors: state.vendors.list,
+  };
 };
 
-export default connect(mapState, actions)(reduxForm({ form: 'paymentForm', validate })(PaymentForm));
+const actions = {
+  closeModal,
+  fetchVendors,
+};
+
+export default connect(
+  mapState,
+  actions
+)(reduxForm({ form: 'paymentForm', validate })(PaymentForm));

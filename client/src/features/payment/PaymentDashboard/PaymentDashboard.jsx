216d0694157363ae41572e3aefd819a5eecf66f3
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2020-05-02 13:54:19 +0800

    add button replaced with SpeedDial

--- client/src/features/payment/PaymentDashboard/PaymentDashboard.jsx
+++ client/src/features/payment/PaymentDashboard/PaymentDashboard.jsx
@@ -4,19 +4,28 @@ import PaymentList from '../PaymentList/PaymentList';
 import PaymentStats from '../PaymentStats/PaymentStats';
 import { openModal } from '../../models/modalActions';
 import { connect } from 'react-redux';
+import { SpeedDial } from '@material-ui/lab';
+import AddIcon from '@material-ui/icons/Add';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  speedDial: {
+    position: 'absolute',
+    '&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft': {
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+  },
+}));
 
 const PaymentDashboard = ({ openModal }) => {
+  const classes = useStyles();
   return (
     <Fragment>
       <Grid>
         <Grid.Row>
           <Grid.Column width={10}>
             <h1>Payments</h1>
-          </Grid.Column>
-          <Grid.Column width={6}>
-            <Button primary floated='right' onClick={ () => openModal('PaymentFormModal') }>
-              <Icon name='plus' /> Add payment
-            </Button>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
@@ -28,6 +37,8 @@ const PaymentDashboard = ({ openModal }) => {
           </Grid.Column>
         </Grid.Row>
       </Grid>
+      <SpeedDial className={classes.speedDial} ariaLabel='Add Payment' open={false} icon={<AddIcon />} onClick={ () => openModal('PaymentFormModal') } />
+
 
       
     </Fragment>
